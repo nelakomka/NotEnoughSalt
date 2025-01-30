0acfa6e5fae097784584af2c6d63d6f3a685c7a5
th667247 <157135550+th667247@users.noreply.github.com>
2025-01-29 21:53:30 -0500

    Updates
    Made a few updates to profile and FAQ and fixed a few CSS items

--- Pages/Profile/Profile.js
+++ Pages/Profile/Profile.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const tabs = document.querySelectorAll('.tab-button');
-  const categories = document.querySelectorAll('.profile-category');
-
-  tabs.forEach((tab) => {
-    tab.addEventListener('click', (event) => {
-      // Remove active class from all tabs
-      tabs.forEach((t) => t.classList.remove('active'));
-
-      categories.forEach((category) => category.classList.remove('active'));
-
-      tab.classList.add('active');
-
-      const categoryId = tab.getAttribute('data-category');
-      const activeCategory = document.getElementById(categoryId);
-
-      if (activeCategory) {
-        activeCategory.classList.add('active');
-      }
+    const tabs = document.querySelectorAll('.tab-button');
+    const categories = document.querySelectorAll('.profile-category');
+  
+    tabs.forEach((tab) => {
+      tab.addEventListener('click', (event) => {
+        // Remove active class from all tabs
+        tabs.forEach((t) => t.classList.remove('active'));
+  
+        categories.forEach((category) => category.classList.remove('active'));
+  
+        tab.classList.add('active');
+  
+        const categoryId = tab.getAttribute('data-category');
+        const activeCategory = document.getElementById(categoryId);
+  
+        if (activeCategory) {
+          activeCategory.classList.add('active');
+        }
+      });
     });
   });
-});
