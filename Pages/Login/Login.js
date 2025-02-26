49af27bfedea934963e082c38a0e1793e9f1bc81
ThrishaArmstrong <122586892+ThrishaArmstrong@users.noreply.github.com>
2025-02-26 13:45:46 -0500

    Updated Login JS
    Removing server js

--- Pages/Login/Login.js
+++ Pages/Login/Login.js
@@ -1,4 +1,4 @@
-const loginForm = document.getElementById('my-login-form');
+const loginForm = document.getElementById("my-login-form");
 
 const login = async (event) => {
   event.preventDefault();
@@ -7,25 +7,24 @@ const login = async (event) => {
   const { email, password } = Object.fromEntries(formData);
 
   try {
-    const response = await fetch('http://localhost:3000/login', {
-      method: 'POST',
+    const response = await fetch("http://localhost:3000/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ email, password }),
     });
 
     if (!response.ok) {
-      throw new Error('Login failed. Please try again.');
+      throw new Error("Login failed. Please try again.");
     }
 
-  // window.location.assign('../Profile/Profile.html'); // Redirect to profile page
+    window.location.assign("../Profile/Profile.html"); // Redirect to profile page
 
-  //window.location.assign('../MealPlan/MealPlan.html');  // Redirect to the meal plan page
-
+    //window.location.assign('../MealPlan/MealPlan.html');  // Redirect to the meal plan page
   } catch (error) {
     console.error(error);
   }
 };
 
-loginForm.addEventListener('submit', login);
+loginForm.addEventListener("submit", login);
